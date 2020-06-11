86aaff342f90328b7f9108a3d92829fa2ecec130
Maxwell <>
2020-06-11 20:48:34 +0800

    feat:开发

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,20 @@
 import _ from 'lodash'
 import './style.css';
+import printMe from './print.js';
 import Icon from './avatar.jpg'
 function component() {
     var element = document.createElement('div');
-  
+    var btn = document.createElement('button');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
     // 将图像添加到我们现有的 div。
     var myIcon = new Image();
     myIcon.src = Icon;
-    console.log('9999999')
-    console.log(myIcon)
     element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
   
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-    entry: './src/index',
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, 'dist')
+    entry: {
+        app:'./src/index',
+        print:'./src/print'
     },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+             contentBase: './dist'
+    },
+    plugins: [
+            // new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
+                title: 'Output Management'
+            })
+        ],
     module: {
         rules: [
             {
